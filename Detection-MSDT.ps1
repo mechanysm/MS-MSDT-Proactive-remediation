442842c13bf0196d40388b9da39a815f918355a4
terryb8s <terryb8s@gmail.com>
2022-06-02 14:42:12 +1200

    Adding detection script
    initial version

---
+++ Detection-MSDT.ps1
@@ -0,0 +1,17 @@
+﻿New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+try
+{
+    if((Test-Path -Path 'HKCR:\ms-msdt') -eq $true) {
+        echo "Registry Key Exists, Remove Key"
+        exit 1
+    }
+    else {
+        echo "Registry Key Does Not Exist, Do Nothing"
+        exit 0
+    }
+}
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
