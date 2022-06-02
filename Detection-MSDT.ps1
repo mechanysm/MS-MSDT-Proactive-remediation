231b0e08c1a1f3313017de3bc4757e2f3cb57625
terryb8s <terryb8s@gmail.com>
2022-06-02 15:00:25 +1200

    Update Detection-MSDT.ps1

--- Detection-MSDT.ps1
+++ Detection-MSDT.ps1
@@ -1,17 +1,20 @@
-﻿New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR -ErrorAction SilentlyContinue
 try
 {
     if((Test-Path -Path 'HKCR:\ms-msdt') -eq $true) {
         echo "Registry Key Exists, Remove Key"
+        Remove-PSDrive -Name HKCR
         exit 1
     }
     else {
         echo "Registry Key Does Not Exist, Do Nothing"
+        Remove-PSDrive -Name HKCR
         exit 0
     }
 }
 catch{
     $errMsg = $_.Exception.Message
     Write-Error $errMsg
+    Remove-PSDrive -Name HKCR
     exit 1
 }
